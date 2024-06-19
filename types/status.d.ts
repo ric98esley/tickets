cb972442e6eb83a8a74536fb6ac2d683948e8bc0
Ricardo Perez <ricardo.pereze.98@gmail.com>
2024-06-18 21:22:53 -0400

    [FETCH] (status.d.ts) add interface

---
+++ types/status.d.ts
@@ -0,0 +1,9 @@
+export interface Status {
+  collectionId:   string;
+  collectionName: string;
+  color:          string;
+  created:        Date;
+  id:             string;
+  name:           string;
+  updated:        Date;
+}
