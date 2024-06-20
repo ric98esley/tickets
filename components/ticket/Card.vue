4fd60a56c9f979d54b0846c0c7759fef25ae288b
Ricardo Perez <ricardo.pereze.98@gmail.com>
2024-06-20 16:31:26 -0400

    [FETCH] (card) add style and data

--- components/ticket/Card.vue
+++ components/ticket/Card.vue
@@ -5,26 +5,6 @@ const props = defineProps(
   {
     data: {
       type: Object as PropType<Ticket>,
-      // default: () => ({
-      //   id: 'Cargando...',
-      //   agentCode: '',
-      //   customerName: '',
-      //   phone: '',
-      //   status: {
-      //     id: '',
-      //     name: '',
-      //     color: ''
-      //   },
-      //   createdBy: {
-      //     id: '',
-      //     username: ''
-      //   },
-      //   assignedTo: {
-      //     id: '',
-      //     username: ''
-      //   },
-      //   createdAt: '1999-01-01T00:00:00.000Z'
-      // }),
     },
   }
 )
@@ -32,43 +12,46 @@ const props = defineProps(
 </script>
 
 <template>
-  <UCard v-if="props.data" class="w-72 h-80" :ui="{ divide: '' }">
+  <UCard v-if="props.data" class="w-72 h-80 lg:w-96 lg:h-[400px] flex-col" :ui="{ divide: '' }">
     <template #header>
-      <div class="flex items-center justify-between">
+      <div class="flex items-center justify-between h-16">
         <div class="flex justify-between items-center overflow-hidden">
-          <span class="text-lg font-semibold">{{ props.data?.agentCode }}</span>
+          <span class="text-lg md:text-2xl lg:text-3xl font-semibold">{{ props.data?.agentCode }}</span>
         </div>
-        <UBadge class="flex items-center p-2" :style="{ ['background-color']: '#' + props.data.status?.color }">
+        <UBadge class="flex items-center text-center p-2 overflow-hidden"
+          :style="{ ['background-color']: '#' + props.data.status?.color }">
           {{ props.data.status?.name }}
         </UBadge>
       </div>
     </template>
-    <h3 class="text-lg font-bold">
-      <span class="text-primary-600 text-base"># {{ props.data?.id.toUpperCase() }}</span>
-    </h3>
-    <div class="mt-2">
-      <p>
-        <span class="text-sm font-light">Cliente: </span>
-        <span class="text-sm font-semibold">{{ props.data.customerName }}</span>
-      </p>
-      <p>
-        <span class="text-sm font-light">Teléfono: </span>
-        <span class="text-sm font-semibold">
-          <a class="text-primary" :href="`tel:${props.data.phone}`">
-            {{ props.data.phone }}
-          </a>
-        </span>
-      </p>
-    </div>
-    <div class="mt-4">
+    <div class="h-28 md:h-28 flex flex-col">
 
+      <h3 class="text-lg font-bold">
+        Soporte: <span class="text-primary-600 text-base"># {{ props.data?.id.toUpperCase() }}</span>
+      </h3>
+      <div class="mt-2">
+        <p>
+          <span class="text-sm font-light">Cliente: </span>
+          <span class="text-sm font-semibold">{{ props.data.customerName }}</span>
+        </p>
+        <p>
+          <span class="text-sm font-light">Teléfono: </span>
+          <span class="text-sm font-semibold">
+            <a class="text-primary" :href="`tel:${props.data.phone}`">
+              {{ props.data.phone }}
+            </a>
+          </span>
+        </p>
+      </div>
+      <div class="mt-4">
+
+      </div>
     </div>
     <template #footer>
-
       <div class="flex items-center justify-between">
         <div class="flex items-center">
           <UAvatar size="xs" :src="props.data.createdBy.avatar" alt="Avatar" class="m-2" />
-          <span class="font-light text-gray-200"> {{ data.createdBy.name }}</span>
+          <span class="font-light text-gray-200"> {{ props.data.createdBy.name }}</span>
         </div>
         <span class="text-sm text-gray-500">{{ timeSince(props.data?.createdAt) }}</span>
       </div>
--- components/ticket/Grid.vue
+++ components/ticket/Grid.vue
@@ -2,14 +2,30 @@
 const props = defineProps({
   data: {
     type: Array as PropType<Ticket[]>,
-    default: () => []
+    default: () => [
+      {
+        id: '1',
+        agentCode: 'AG-001',
+        status: {
+          name: 'Abierto',
+          color: 'FFA500',
+        },
+        customerName: 'John Doe',
+        phone: '1234567890',
+        createdBy: {
+          avatar: 'https://randomuser.me/api/portraits',
+          name: 'Jane Doe',
+        },
+        createdAt: new Date(),
+      },
+    ]
   }
 })
 
 </script>
 
 <template>
-  <div class="grid grid-cols-1 gap-3 md:grid-cols-2 lg:grid-cols-3">
+  <div class="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
     <TicketCard v-for="ticket in props.data" :key="ticket.id" :data="ticket" />
   </div>
 </template>
